f2b12f19dea4fe66b1091451a2425685c7c78a08
joko1931 <okoonja@mail.uc.edu>
2023-09-30 18:08:17 -0400

    final commit

--- src/Main.java
+++ src/Main.java
@@ -4,62 +4,47 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         int guess = 0;
-        boolean numberDone = false;
-        boolean done = false;
-        boolean playAgain = false;
-        String trash = "";
-        String numTrash = "";
-        int oneZero = 0;
+        boolean inRange;
         int min = 1;
         int max = 10;
 
-        do
-        {
+            inRange = false;
             System.out.println("Guess a number [1-10]: ");
             do
             {
                 if (in.hasNextInt())
                 {
                     guess = in.nextInt();
-                        if (guess >= 1 && guess <= 10)
-                        {
-                            numberDone = true;
-                            int random = (int)Math.floor(Math.random() * (max - min + 1) + min);
 
-                            if (guess > random)
-                            {
-                                System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was too high.");
-                            }
-                            else if (guess < random)
-                            {
-                                System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was too low.");
-                            }
-                            else
-                            {
-                                System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was on the money!");
-                            }
-                        }
-                        else
-                        {
-                            numTrash = in.nextLine();
-                            System.out.println("While " + numTrash + " is a number, it isn't in the range provided. Please try again.");
-                        }
+                    if (guess >= 1 && guess <= 10)
+                    {
+                        inRange = true;
+                    }
+                    else
+                    {
+                        System.out.println(guess + " isn't in the range listed. Please input value in the range.");
+                    }
                 }
                 else
                 {
-                    trash = in.nextLine();
-                    System.out.println("Please enter valid value, not " + trash);
+                    System.out.println("Please enter valid input.");
+                    in.nextLine();
                 }
-            }while(!numberDone);
-                System.out.println("Do you want to play again [1/0]?");
-                if (in.hasNextInt())
+            }while (!inRange);
+
+                int random = (int) Math.floor(Math.random() * (max - min + 1) + min);
+
+                if (guess > random)
                 {
-                    oneZero = in.nextInt();
-                    if (oneZero == 0)
-                    {
-                        done = true;
-                    }
+                    System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was too high.");
                 }
-        }while(!done);
+                else if (guess < random)
+                {
+                    System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was too low.");
+                }
+                else
+                {
+                    System.out.println("Your guess was " + guess + " and the random number was " + random + ". Your guess was on the money!");
+                }
     }
 }
